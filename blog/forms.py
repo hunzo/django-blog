50e4bda0001b44a3963479d40c6d96ebb6a4549f
hunzo <takraw_s@hotmail.com>
2022-05-13 11:43:51 +0700

    update

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,9 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'content')
+        fields = ('title', 'author', 'content', 'created_by')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'created_by': forms.Select(attrs={'class': 'form-select'}),
         }
